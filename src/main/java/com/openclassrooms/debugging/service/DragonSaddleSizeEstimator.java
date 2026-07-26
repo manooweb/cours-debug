ac89bd52b9cd72519cbc7bba5df4f739edf3d816
Emmanuel Hesry <ehesry@gmail.com>
2026-07-26 12:45:36 +0200

    Report des loggers dans la classe DragonSaddleSizeEstimator

--- src/main/java/com/openclassrooms/debugging/service/DragonSaddleSizeEstimator.java
+++ src/main/java/com/openclassrooms/debugging/service/DragonSaddleSizeEstimator.java
@@ -1,6 +1,8 @@
 package com.openclassrooms.debugging.service;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class DragonSaddleSizeEstimator {
+    private static Logger logger = LoggerFactory.getLogger(DragonSaddleSizeEstimator.class);
 
     /**
      * A mystical constant
@@ -27,23 +30,28 @@ public class DragonSaddleSizeEstimator {
      * @return Saddle size
      */
     public Double estimateSaddleSizeInCentiMeters(int targetYear) throws Exception {
+        logger.debug("Estimation d'une taille de selle pour {}", targetYear);
         double saddleSizeInCm = calculateSaddleSizeFromYear(targetYear);
 
         // Verify that we have a valid saddle size
         verifier.verify(saddleSizeInCm);
+        logger.info("Taille de selle en cm calculée: {}", saddleSizeInCm);
 
         return saddleSizeInCm;
     }
 
     private double calculateSaddleSizeFromYear(int targetYear) {
+        logger.trace("A propos du calcul d'une taille de selle avec la constante mystique: {} et l'année cible: {}", MYSTICAL_CONSTANT, targetYear);
         // Count down the number of years it's been alive
         double saddleSizeInCm = targetYear-MYSTICAL_CONSTANT;
 
         // This is from an era before dragon's were spawned
         if (saddleSizeInCm<0) {
+            logger.warn("Taille de selle inférieure à 0 détectée");
             return NON_EXISTANT_DRAGON_SIZE;
         }
 
+        logger.trace("Taille de selle calculée {}cm avec la constante mystique[{}] et l'année cible[{}]", saddleSizeInCm, MYSTICAL_CONSTANT, targetYear);
         // calculate the final saddle size
         return saddleSizeInCm;
     }
